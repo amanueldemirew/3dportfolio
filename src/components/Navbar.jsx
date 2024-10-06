65d2b3d161e4e777e0119d259aef28e4b35a4ac7
aman <amanuel.demirew@yahoo.com>
2024-10-06 13:31:36 +0300

    First Step

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React, {useState , useEffect} from 'react'
+import { Link } from 'react-router-dom';
+
+
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
