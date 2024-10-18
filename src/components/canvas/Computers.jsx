c78e723676b0438559992817586b1f1b6bd91b97
Amanuel Demirew <amanuel.demirew@yahoo.com>
2024-10-18 14:22:58 +0300

    Update Computers.jsx

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,6 +1,7 @@
 import React, { Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+
 import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
@@ -8,22 +9,21 @@ const Computers = ({ isMobile }) => {
 
   return (
     <mesh>
-      <hemisphereLight intensity={0.6} groundColor="black" />
-
+      <hemisphereLight intensity={0.15} groundColor='black' />
       <spotLight
-        position={[-20, 200, 10]}
+        position={[-20, 50, 10]}
         angle={0.12}
         penumbra={1}
-        intensity={45000}
+        intensity={1}
         castShadow
         shadow-mapSize={1024}
       />
       <pointLight intensity={1} />
       <primitive
         object={computer.scene}
-        scale={isMobile ? 0.4 : 0.75}
-        position={isMobile ? [-4.7, -2.2, -2.2] : [0, -4.3, -1.5]}
-        rotation={isMobile ? [.01, -0.2, -0.1] : [-0.01, -0.2, -0.1]}
+        scale={isMobile ? 0.7 : 0.75}
+        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
   );
@@ -55,10 +55,10 @@ const ComputersCanvas = () => {
 
   return (
     <Canvas
-      frameloop="demand"
+      frameloop='demand'
       shadows
       dpr={[1, 2]}
-      camera={{ position: [20, 3, 5], fov: 32 }}
+      camera={{ position: [20, 3, 5], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
