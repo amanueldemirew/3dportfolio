95743499d3d36167b1a89009c75d9aba7dc6938c
Amanuel Demirew <amanuel.demirew@yahoo.com>
2024-10-31 08:23:40 +0300

    reverse the update

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,7 +1,6 @@
 import React, { Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
-
 import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
@@ -9,21 +8,22 @@ const Computers = ({ isMobile }) => {
 
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor='black' />
+      <hemisphereLight intensity={0.6} groundColor="black" />
+
       <spotLight
-        position={[-20, 50, 10]}
+        position={[-20, 200, 10]}
         angle={0.12}
         penumbra={1}
-        intensity={1}
+        intensity={45000}
         castShadow
         shadow-mapSize={1024}
       />
       <pointLight intensity={1} />
       <primitive
         object={computer.scene}
-        scale={isMobile ? 0.7 : 0.75}
-        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
-        rotation={[-0.01, -0.2, -0.1]}
+        scale={isMobile ? 0.4 : 0.75}
+        position={isMobile ? [-4.7, -2.2, -2.2] : [0, -4.3, -1.5]}
+        rotation={isMobile ? [.01, -0.2, -0.1] : [-0.01, -0.2, -0.1]}
       />
     </mesh>
   );
@@ -36,7 +36,7 @@ const ComputersCanvas = () => {
     // Add a listener for changes to the screen size
     const mediaQuery = window.matchMedia("(max-width: 500px)");
 
-    // Set the initial value of the `isMobile` state variable
+    // Set the initial value of the isMobile state variable
     setIsMobile(mediaQuery.matches);
 
     // Define a callback function to handle changes to the media query
@@ -55,10 +55,10 @@ const ComputersCanvas = () => {
 
   return (
     <Canvas
-      frameloop='demand'
+      frameloop="demand"
       shadows
       dpr={[1, 2]}
-      camera={{ position: [20, 3, 5], fov: 25 }}
+      camera={{ position: [20, 3, 5], fov: 32 }}
       gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
